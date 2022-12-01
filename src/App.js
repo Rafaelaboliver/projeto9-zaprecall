ca82ecd387e95323d0a2f8fd8b6fb3594d256414
rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2022-12-01 18:56:22 -0300

    componentes criados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import styled from 'styled-components'
+import Pergunta from './components/Pergunta';
+import Deck from './components/Deck';
+import Footer from './components/Footer';
+
 export default function App() {
+
   return (
-    <div className="App">
-      
+    <div className="screen-container">
+      <Pergunta/>
+      <Deck/>
+      <Footer/>
     </div>
   );
 }
